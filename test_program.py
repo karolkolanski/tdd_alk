f31e8405383a9bcb0f8575180ae67858706ae827
Karol Kolanski <gralu86@gmail.com>
2022-01-23 13:48:14 +0100

    update of test_is_adult

--- test_program.py
+++ test_program.py
@@ -6,9 +6,14 @@ class TestCustomer(unittest.TestCase):
         pass
     def tearDown(self):
         pass
+
     def test_is_adult(self):
         # Stworzyć instancję klienta niepełnoletniego 17
+        customer17 = Customer("John", "Smith", 17)
+        self.assertEqual(False, customer17.is_adult())
         # Stworzyć instancję klienta pełnoletniego 18
+        customer18 = Customer("Anna", "Black", 18)
+        self.assertEqual(True, customer18.is_adult())
         # Stworzyć instancję klienta pełnoletniego 19
         # sprawdzic is_adult() na kazdej instancji
 
